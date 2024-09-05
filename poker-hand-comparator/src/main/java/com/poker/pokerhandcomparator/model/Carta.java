1a2c1e7f1595cdda1a288f83025029a229069f78
Alvarezzz14 <andresalvarez14@gmail.com>
2024-09-05 16:40:16 -0500

    Model Carta

--- poker-hand-comparator/src/main/java/com/poker/pokerhandcomparator/model/Carta.java
+++ poker-hand-comparator/src/main/java/com/poker/pokerhandcomparator/model/Carta.java
@@ -1,4 +1,13 @@
 package com.poker.pokerhandcomparator.model;
 
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
 public class Carta {
+
+    private String valor;
+    private String palo;
+
 }
