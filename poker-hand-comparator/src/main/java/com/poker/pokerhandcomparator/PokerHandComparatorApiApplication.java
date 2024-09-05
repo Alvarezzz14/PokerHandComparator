cddce2e07ac01fb9f5d921ac2ffb9174b27d6b50
Alvarezzz14 <andresalvarez14@gmail.com>
2024-09-05 15:43:04 -0500

    Initial commit

---
+++ poker-hand-comparator/src/main/java/com/poker/pokerhandcomparator/PokerHandComparatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.poker.pokerhandcomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerHandComparatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerHandComparatorApiApplication.class, args);
+	}
+
+}
